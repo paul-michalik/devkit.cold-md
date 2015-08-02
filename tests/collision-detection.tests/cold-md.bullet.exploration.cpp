f918f6162679bd7bbc3650138f0619b416598c3b
Paul Michalik <paul.michalik@outlook.com>
2015-08-02 12:31:29 +0200

    Renamed files and projects to match the content.

--- tests/collision-detection.tests/cold-md.bullet.exploration.cpp
+++ tests/collision-detection.tests/cold-md.bullet.exploration.cpp
@@ -1,27 +1,16 @@
 #include "stdafx.h"
 #include "CppUnitTest.h"
+#include <cold-md.contract/cold-md.h>
+#include <cold-md.bullet/world.h>
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
-namespace cold {
-
-    namespace contract {
-        struct world {
-            virtual world* add_object() = 0;
-        protected:
-            ~world() = default;
-        };
-    }
-
-    std::shared_ptr<contract::world> create_world();
-}
-
 namespace tests {
-    namespace collision_detection {
-        TEST_CLASS(test_class)
+    namespace cold_md_bullet_exploration {
+        TEST_CLASS(test_suite)
         {
             BEGIN_TEST_CLASS_ATTRIBUTE()
-                TEST_CLASS_ATTRIBUTE(L"Module", L"tests.cd.bullet_try_outs")
+                TEST_CLASS_ATTRIBUTE(L"Module", L"cold-md.bullet.exploration")
                 END_TEST_CLASS_ATTRIBUTE();
 
             MSTEST_UTILS_TRACKED_MEM_CHECK();
@@ -110,63 +99,14 @@ namespace tests {
                 Assert::Fail(L"not implemented");
             }
 
-            TEST_METHOD(can_construct_and_initialize_cold_world)
+            TEST_METHOD(can_construct_and_initialize_cold_bullet_world)
             {
-                auto t_world = cold::create_world();
-
-                Assert::IsTrue(!!t_world);
-
-                Logger::WriteMessage(mstest_utils::wlog_message()
-                    << "cold destroyed!"
-                    << std::endl | mstest_utils::as_string);
-            }
-        };
-    }
-}
-
-namespace cold {
-    namespace bullet_impl {
-        class world : public ::cold::contract::world { 
-            std::unique_ptr<btCollisionWorld> m_col_world;
-            std::unique_ptr<btDbvtBroadphase> m_col_broadphase;
-            std::unique_ptr<btDefaultCollisionConfiguration> m_col_configuration;
-            std::unique_ptr<btCollisionDispatcher> m_col_dispatcher;
-
-            virtual ::cold::contract::world* add_object() override
-            {
-                return this;
-            }
-        public:
-            world()
-            {
-                m_col_broadphase =
-                    std::make_unique<btDbvtBroadphase>();
-
                 {
-                    m_col_configuration =
-                        std::make_unique<btDefaultCollisionConfiguration>();
+                    auto t_world = std::make_unique<cold::bullet::world>();
 
-                    {
-                        m_col_dispatcher =
-                            std::make_unique<btCollisionDispatcher>(m_col_configuration.get());
-
-                        btGImpactCollisionAlgorithm::registerAlgorithm(m_col_dispatcher.get());
-
-                        {
-                            m_col_world =
-                                std::make_unique<btCollisionWorld>(
-                                m_col_dispatcher.get(),
-                                m_col_broadphase.get(),
-                                m_col_configuration.get());
-                        }
-                    }
+                    Assert::IsTrue(!!t_world);
                 }
             }
         };
-    }
-
-    std::shared_ptr<contract::world> create_world()
-    {
-        return std::make_unique<bullet_impl::world>();
     }
 }
